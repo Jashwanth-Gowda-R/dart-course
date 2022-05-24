8d5b1a3255193795f5a52fba9e6628c1a1266df8
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-05-24 15:23:11 +0530

    added checkout fn

--- simple_ecommerce/checkout.dart
+++ simple_ecommerce/checkout.dart
@@ -38,7 +38,9 @@ void main() {
     } else if (input == 'v') {
       print(cart);
     } else if (input == 'c') {
-      exit(1);
+      if (checkout(cart)) {
+        break;
+      }
     }
   }
 }
@@ -109,4 +111,30 @@ Product? chooseProduct() {
   }
   print('Not found');
   return null;
+}
+
+bool checkout(Cart cart) {
+  if (cart.isEmpty) {
+    print('Cart is empty');
+    return false;
+  }
+  final total = cart.total();
+  print('Total: \$$total');
+  stdout.write('Payment in cash: ');
+  final line = stdin.readLineSync();
+  if (line == null || line.isEmpty) {
+    return false;
+  }
+  final paid = double.tryParse(line);
+  if (paid == null) {
+    return false;
+  }
+  if (paid >= total) {
+    final change = paid - total;
+    print('Change: \$${change.toStringAsFixed(2)}');
+    return true;
+  } else {
+    print('Not enough cash.');
+    return false;
+  }
 }
